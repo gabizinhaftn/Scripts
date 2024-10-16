af745347f09d788d46a104529006db98ac450479
gabizinhaftn <gabizinhaftn>
2024-10-16 16:27:12 -0300

    scripts

---
+++ Tabela RAE Atendimento.sql
@@ -0,0 +1,19 @@
+USE HubDados
+;
+
+SELECT TOP (10) * FROM RAE.Atendimento --IdAtendimento, IdLocalAtendimento, Centro Custo, Tipo Atendimento, Canal
+
+SELECT TOP (10) * FROM RAE.Atendimento_AtividadeSala --IdAtendimento
+
+SELECT TOP (10) * FROM RAE.Atendimento_ConteudoRelacionado --IdAtendimento
+
+SELECT TOP (10) * FROM RAE.Atendimento_Ministrante --IdAtendimento, IdFuncionario
+
+SELECT TOP (10) * FROM RAE.Atendimento_Produto --IdAtendimento
+
+
+SELECT TOP (10) * FROM RAE.Acao --IdAcao, Centro Custo
+
+SELECT TOP (10) * FROM RAE.Fornecedor --IdPessoa
+
+SELECT TOP (10) * FROM RAE.Funcionario --IdFuncionario, IdPessoa
