b8b9576d95f8519346d569e509ce681524038e1c
gabizinhaftn <gabiruela11@gmail.com>
2025-01-31 10:10:13 -0300

    sql

--- SQL Server/11MonitoramentoPosicao.sql
+++ SQL Server/11MonitoramentoPosicao.sql
@@ -3,12 +3,13 @@ USE FINANCA
 
 UPDATE dbo.MONITORAMENTO
 SET Posição = 'Pedido de fornecimento – encerrado'
-WHERE [Cod.Contrato] = 'SPA4.000074.24'
+WHERE [Cod.Contrato] = 'SPB1.000306.24'
 ;
 
 UPDATE dbo.Posicao
 SET Posição = 'Pedido de fornecimento – encerrado'
-WHERE [Cod.Contrato] = 'SPA4.000074.24'
+WHERE [Cod.Contrato] = 'SPB1.000306.24'
+
 
 /*INSERT INTO Posicao
-VALUES ('SPA4.000074.24', '', 'Pedido de fornecimento – encerrado')*/
+VALUES ('SPB1.000306.24', '', 'Pedido de fornecimento – encerrado')*/
